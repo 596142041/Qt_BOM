88d74409714fd03e333e7c22cc57a1d033f488ee
ad596142041 <wcj53636746@126.com>
2024-02-27 21:10:03 +0800

    1.修改结构体成员名称 2.添加部分全局变量; 3.删除未使用的函数

--- mainwindow.h
+++ mainwindow.h
@@ -23,44 +23,47 @@
 #include <QTableWidgetItem>
 #include <QDateTime>
 #include <QFileInfo>
-
-//---
+//Excel库
 #include "xlsxdocument.h"
-#include "xlsxchartsheet.h"
 #include "xlsxcellrange.h"
-#include "xlsxchart.h"
 #include "xlsxrichstring.h"
 #include "xlsxworkbook.h"
+
+//-----------------------
 #include "qstring_cmp.h"
-
 #include "json_resolve.h"
-
 #include "LogHandler.h"
 //---------------------
-typedef enum _COLUMN_HEAD_INDEX {
-    Model_Name_B    = 1,
-    Factory_B       = 2,
-    Description_B   = 3,
-    Point_B         = 4,
-    Quantity_B      = 5,
-    Model_Name_A    = 6,
-    Factory_A       = 7,
-    Description_A   = 8,
-    Point_A         = 9,
-    Quantity_A      = 10,
-    Change_type     = 11
+typedef enum _COLUMN_HEAD_INDEX
+{
+    Change_date     = 1,
+    Indx_cnt        = 2,//待修改
+    Model_Name_B    = 3,
+    Factory_B       = 4,
+    Description_B   = 5,
+    Point_B         = 6,
+    Quantity_B      = 7,
+    Model_Name_A    = 8,
+    Factory_A       = 9,
+    Description_A   = 10,
+    Point_A         = 11,
+    Quantity_A      = 12,
+    Change_type     = 13
 }COLUMN_HEAD_INDEX;
 //列宽
-typedef enum _COLUMN_With {
-    Model_Name_With = 30,
+typedef enum _BMP_COLUMN_Width
+{
+    Quantity_With = 7,
+    Indx_Width = 8,
+    Description_With = 11,
+    Change_type_With = 16,
+    Date_Width = 17,
     Factory_With = 17,
-    Description_With = 20,
-    Point_With = 90,
-    Quantity_With = 9,
-    Change_type_With = 16
+    Model_Name_With = 30,
+    Point_With = 90
 }COLUMN_With;
 //Excel中各项列号
-typedef enum _Excel_Column_INDEX
+typedef enum _BOM_Column_INDEX
 {
     Quantity_Column = 3,
     Point_Column = 4,
@@ -70,6 +73,7 @@ typedef enum _Excel_Column_INDEX
     Factory1_Column = 8,
     Column_OFFSET =2
 }Excel_Column_INDEX;
+#define CONFIG_NAME "config.json" //配置文件名
 //---------------------------
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -82,12 +86,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    QString Read_colum(const QString Name);
-    QString Read_colum_All(const QString File_Name,int start_row,int column);
     QStringList Read_colum_List(const QString File_Name,int start_row,int column);
-    QString Read_cell(const QString Name,int row, int column);
-    int Get_Row(const QString File_Name,const QString str,int column);
-
 private slots:
     void on_pushButton_open_clicked();
     void on_pushButton_open_old_clicked();
@@ -98,14 +97,16 @@ private:
     Ui::MainWindow *ui;
     QString File_Name_New;
     QString File_Name_Old;
-    void Excel_SetTitle(QXlsx::Document *pDocument);
     Qstring_cmp *str_cmp;
     QTableWidgetItem *cell_Item;
     Json_resolve *json;
     void Excel_update();
-    QXlsx::Document *Write_xlsx;
     QString Write_xlsx_name;
-    bool tst_btn_enable = false;
-    bool log_enable = false;
+    bool tst_btn_enable;
+    bool log_enable;
+    int write_row;
+    QXlsx::Document *Write_xlsx;
+    QXlsx::Document *Read_New_BOM;
+    QXlsx::Document *Read_Old_BOM;
 };
 #endif // MAINWINDOW_H
